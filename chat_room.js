90ca0fb9349feedad3b42cb295c1e80ef521098f
codeytimey <87842664+codeytimey@users.noreply.github.com>
2021-09-27 10:56:27 +1000

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCcFs4q4UNlZN34L5A5r8Gp-S3t3P2p6xg",
+    authDomain: "lets-chat-web-app-1b382.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-1b382-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-1b382",
+    storageBucket: "lets-chat-web-app-1b382.appspot.com",
+    messagingSenderId: "82256188255",
+    appId: "1:82256188255:web:345c7fb75e5396ab239cea"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
